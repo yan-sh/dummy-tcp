d6a97f645a57b8c31c86ce157a85f8a991e55691
Yan Shkurinsky <yan.shkurinsky@gmail.com>
2021-01-17 12:07:39 +0400

    Add connection counters

--- MyLib.hs
+++ MyLib.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE LambdaCase #-}
 
 module MyLib (toBS, run) where
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC8
 import qualified Data.ByteString.Lazy as BL
+import Data.Functor ((<&>))
+import Data.Bool (bool)
 import Data.Int
 import Data.Binary
 import Network.Socket
@@ -14,12 +19,9 @@ import qualified Control.Exception as E
 import Control.Monad (forever, unless, void)
 import Control.Concurrent.Async (async)
 import Network.Socket.ByteString (recv, sendAll)
-
-toBS :: String -> B.ByteString
-toBS s = BL.toStrict $ mconcat
-  [ encode (fromIntegral (length s) :: Int32)
-  , BL.fromStrict $ BC8.pack s
-  ]
+import Control.Concurrent.STM
+import Control.Concurrent (ThreadId, threadDelay, forkIO)
+import System.IO.Unsafe (unsafePerformIO)
 
 port :: Int
 port = 9922
@@ -30,6 +32,29 @@ socketMaxQueue = 1024
 socketMaxRec :: Int
 socketMaxRec = 1024
 
+acceptMessage :: B.ByteString
+acceptMessage = toBS "HELLO"
+
+receiveMessage :: B.ByteString
+receiveMessage = toBS "HI"
+
+totalConn :: TVar Int
+totalConn = unsafePerformIO $ newTVarIO 0
+{-# NOINLINE totalConn #-}
+
+activeConn :: TVar Int
+activeConn = unsafePerformIO $ newTVarIO 0
+{-# NOINLINE activeConn #-}
+
+
+data ConnectionStatus = Active | Inactive
+  deriving Eq
+
+data Connection = Connection
+  { status :: TVar ConnectionStatus
+  , socket :: Socket
+  }
+
 
 prepareSocket :: Socket -> AddrInfo -> IO Socket
 prepareSocket socket addr@AddrInfo{..} = do
@@ -39,8 +64,18 @@ prepareSocket socket addr@AddrInfo{..} = do
   pure socket
 
 
+debugLogger :: IO ()
+debugLogger = do
+  total <- readTVarIO totalConn
+  active <- readTVarIO activeConn
+  putStrLn $ mconcat ["total - ", show total, ", active - ", show active]
+  threadDelay 1_000_000
+  debugLogger
+
+
 run :: IO ()
 run = withSocketsDo do
+  forkIO debugLogger
   addr <- head <$> getAddrInfo
     do Just defaultHints {addrSocketType = Stream}
     do Just "127.0.0.1"
@@ -50,14 +85,61 @@ run = withSocketsDo do
 
 
 loop :: Socket -> IO ()
-loop socket = do
-  forever $ E.bracketOnError (fst <$> accept socket) close \conn -> do
+loop socket' = do
+  forever $ E.bracketOnError (newConn socket') closeConn \conn -> do
     void $ async do
-      sendAll conn $ toBS "HELLO"
-      go conn where
+      withSocket conn (`sendAll` acceptMessage)
+      go conn
+      closeConn conn
+      where
         go conn = do
-          msg <- recv conn socketMaxRec
+          msg <- withSocket conn (`recv` socketMaxRec)
           unless (B.null msg) do
-            sendAll conn $ toBS "HI"
+            activateConn conn
+            withSocket conn (`sendAll` receiveMessage)
             go conn
-          close conn
+
+
+toBS :: String -> B.ByteString
+toBS s = BL.toStrict $ mconcat
+  [ encode @Int32 $ fromIntegral $ length s
+  , BL.fromStrict $ BC8.pack s
+  ]
+
+withSocket :: Connection -> (Socket -> IO a) -> IO a
+withSocket Connection{..} f = f socket
+
+newConn :: Socket -> IO Connection
+newConn socket' = do
+  incTotalConn
+  socket <- fst <$> accept socket'
+  status <- newTVarIO Inactive
+  pure Connection {..}
+
+activateConn :: Connection -> IO ()
+activateConn c@Connection{..} = connIsActive c >>= bool
+  do atomically (writeTVar status Active) >> incActiveConn
+  do pure ()
+
+connIsActive :: Connection -> IO Bool
+connIsActive Connection{..} = readTVarIO status <&> (== Active)
+
+closeConn :: Connection -> IO ()
+closeConn c@Connection{..} = do
+  connIsActive c >>= bool
+    do pure ()
+    do decActiveConn
+  decTotalConn
+  close socket
+
+incTotalConn :: IO ()
+incTotalConn = atomically $ modifyTVar' totalConn (+1)
+
+decTotalConn :: IO ()
+decTotalConn = atomically $ modifyTVar' totalConn (\x -> x -1)
+
+incActiveConn :: IO ()
+incActiveConn = atomically $ modifyTVar' activeConn (+1)
+
+decActiveConn :: IO ()
+decActiveConn = atomically $ modifyTVar' activeConn (\x -> x -1)
